9ec4757fb1fd7fa37f663a2248c3ebb222930a1e
Dmitry Astapov <dastapov@gmail.com>
2020-02-18 22:09:54 +0000

    Check that container is viable after build

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ docker image pull haskell
 docker image build -t dastapov/hledger:latest-dev -t dastapov/hledger:${v}-dev --target dev .
 docker image build -t dastapov/hledger:latest -t dastapov/hledger:${v} .
 
+./run.sh ./data/hledger.journal hledger print || { echo "failed to run container, aborting"; exit 1; }
+
 docker image push dastapov/hledger:${v}-dev
 docker image push dastapov/hledger:latest-dev
 docker image push dastapov/hledger:${v}
