d3021d307714078588a4c0c7e84a43c0c8ff39d1
Dmitry Astapov <dastapov@gmail.com>
2020-02-18 14:15:32 +0000

    hledger-api is no more

--- start.sh
+++ start.sh
@@ -2,17 +2,11 @@
 
 echo "host: ${HLEDGER_HOST:=0.0.0.0}"
 echo "port: ${HLEDGER_PORT:=5000}"
-echo "port: ${HLEDGER_API_PORT:=5001}"
 echo "base url: ${HLEDGER_BASE_URL:="http://localhost:$HLEDGER_PORT"}"
 echo "file url: ${HLEDGER_FILE_URL:=}"
 echo "input file: ${HLEDGER_JOURNAL_FILE:=/data/hledger.journal}"
 echo "debug level: ${HLEDGER_DEBUG:=1}"
 echo "rules file: ${HLEDGER_RULES_FILE:=/data/hledger.rules}"
-
-hledger-api \
-    --file "$HLEDGER_JOURNAL_FILE" \
-     --host=$HLEDGER_HOST \
-     --port=$HLEDGER_API_PORT &
     
 exec hledger-web \
      --server \
