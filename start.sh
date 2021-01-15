f970c9707a2581ab3dc4cbb6e09686e73b2ab8b6
Fabian Schuh <mail@xeroc.org>
2021-01-15 14:05:03 +0100

    Add capabilities

--- start.sh
+++ start.sh
@@ -7,13 +7,15 @@ echo "file url: ${HLEDGER_FILE_URL:=}"
 echo "input file: ${HLEDGER_JOURNAL_FILE:=/data/hledger.journal}"
 echo "debug level: ${HLEDGER_DEBUG:=1}"
 echo "rules file: ${HLEDGER_RULES_FILE:=/data/hledger.rules}"
+echo "capabilities: ${HLEDGER_CAPABILITIES:=view,add}"
     
 exec hledger-web \
      --server \
      --host=$HLEDGER_HOST \
      --port=$HLEDGER_PORT \
-     --file "$HLEDGER_JOURNAL_FILE" \
+     --file="$HLEDGER_JOURNAL_FILE" \
      --debug=$HLEDGER_DEBUG \
      --base-url=$HLEDGER_BASE_URL \
      --file-url=$HLEDGER_FILE_URL \
-     --rules-file="$HLEDGER_RULES_FILE"
+     --rules-file="$HLEDGER_RULES_FILE \
+     --capabilities=$HLEDGER_CAPABILITIES"
