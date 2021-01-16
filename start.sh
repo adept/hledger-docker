cefb510ea0453f6b10232eeb16487f16e5862b1b
Fabian Schuh <mail@xeroc.org>
2021-01-16 15:20:33 +0100

    Add extra_args

--- start.sh
+++ start.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-echo "host: ${HLEDGER_HOST:=0.0.0.0}"
-echo "port: ${HLEDGER_PORT:=5000}"
-echo "base url: ${HLEDGER_BASE_URL:="http://localhost:$HLEDGER_PORT"}"
-echo "file url: ${HLEDGER_FILE_URL:=}"
-echo "input file: ${HLEDGER_JOURNAL_FILE:=/data/hledger.journal}"
-echo "debug level: ${HLEDGER_DEBUG:=1}"
-echo "rules file: ${HLEDGER_RULES_FILE:=/data/hledger.rules}"
+echo "host:         ${HLEDGER_HOST:=0.0.0.0}"
+echo "port:         ${HLEDGER_PORT:=5000}"
+echo "base url:     ${HLEDGER_BASE_URL:="http://localhost:$HLEDGER_PORT"}"
+echo "file url:     ${HLEDGER_FILE_URL:=}"
+echo "input file:   ${HLEDGER_JOURNAL_FILE:=/data/hledger.journal}"
+echo "debug level:  ${HLEDGER_DEBUG:=1}"
+echo "rules file:   ${HLEDGER_RULES_FILE:=/data/hledger.rules}"
 echo "capabilities: ${HLEDGER_CAPABILITIES:=view,add}"
-    
+echo "extra_args:   ${HLEDGER_ARGS:=''}"
+echo "---------------------------------------------------------------"
+
 exec hledger-web \
      --server \
      --host=$HLEDGER_HOST \
@@ -17,5 +19,6 @@ exec hledger-web \
      --debug=$HLEDGER_DEBUG \
      --base-url=$HLEDGER_BASE_URL \
      --file-url=$HLEDGER_FILE_URL \
-     --rules-file="$HLEDGER_RULES_FILE \
-     --capabilities=$HLEDGER_CAPABILITIES"
+     --rules-file="$HLEDGER_RULES_FILE" \
+     --capabilities=$HLEDGER_CAPABILITIES \
+     $HLEDGER_ARGS
