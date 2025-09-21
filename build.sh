64ca45ff13ecba420537cd7ef9d0c5a5547e16ad
Dmitry Astapov <dastapov@gmail.com>
2025-09-21 14:30:28 +0100

    1.50.1

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-v=1.50
+v=1.50.1
 docker image build -t dastapov/hledger:latest-dev -t dastapov/hledger:${v}-dev --target dev . "$@"
 docker image build -t dastapov/hledger:latest -t dastapov/hledger:${v} . "$@"
 
